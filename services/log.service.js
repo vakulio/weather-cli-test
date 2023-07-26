d4c5ed98e18c0050c77892b3a9c01f86272f767f
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-07-26 18:18:27 +0300

    add linter

--- services/log.service.js
+++ services/log.service.js
@@ -1,26 +1,24 @@
 import chalk from "chalk";
+
 import dedent from "dedent";
 
 const printError = (error) => {
-    console.log(chalk.bgRed("Error") + ' ' + error);
-
-}
-
+  console.log(chalk.bgRed("Error") + " " + error);
+};
 
 const printSuccess = (message) => {
-    console.log(chalk.bgBlue("Success") + ' ' + message);
-}
-
+  console.log(chalk.bgBlue("Success") + " " + message);
+};
 
 const printHelp = () => {
-    console.log(
-    dedent`${chalk.bgCyanBright("  HELP  ")} 
+  console.log(
+    dedent`${chalk.bgCyanBright("  HELP  ")}
     Без параметров - вывод погоды
     -s [CITY] для установки Города
     -h для вывода помощи
     -token [API_KEY] для сохранения токена
-    `);
-}
+    `
+  );
+};
 
-
-export { printError, printSuccess, printHelp }
+export { printError, printSuccess, printHelp };
--- weather.js
+++ weather.js
@@ -1,24 +1,19 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
-import { printHelp } from './services/log.service.js';
+import { printHelp } from "./services/log.service.js";
+import { program } from "commander";
 
 const initCLI = () => {
-    program
-        .option('-s, --town <char>')
-        .option('-h, --help')
-        .option('-token, --token <char>')
-    program.parse();
-    const options = program.opts();
-    if (options.help) {
-        printHelp()
-    } 
-    if (options.town) {
-        console.log(`Save ${options.town}`);
-    }
-    if (options.token) {
-        console.log(`Save ${options.token}`);
-    }
+  program
+    .option("-s, --town <char>")
+    .option("-h, --help")
+    .option("-token, --token <char>");
+  program.parse();
+  const options = program.opts();
+  if (options.help) printHelp();
 
-}
-initCLI()
+  if (options.town) console.log(`Save ${options.town}`);
+
+  if (options.token) console.log(`Save ${options.token}`);
+};
+initCLI();
